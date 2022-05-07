436d2fa2f9590185980a21ffb36d38f01d7ecc83
Josue Solis <josue1dario2solis3@gmail.com>
2022-05-07 17:31:08 -0300

    add anotation @PostMapping

--- src/main/java/com/backend/loginAndRegister/registration/RegistrationRequest.java
+++ src/main/java/com/backend/loginAndRegister/registration/RegistrationRequest.java
@@ -1,9 +1,6 @@
 package com.backend.loginAndRegister.registration;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
